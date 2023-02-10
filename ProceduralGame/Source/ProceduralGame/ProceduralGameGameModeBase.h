f023243215882108937dca75a253a3ff1fbcbf04
MaxLichtneker <maxwil2001@hotmail.com>
2023-02-10 11:46:20 +0100

    initial commit

---
+++ ProceduralGame/Source/ProceduralGame/ProceduralGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALGAME_API AProceduralGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
